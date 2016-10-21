df24bf41388a0cb0f675a3de8a3976778a89c162
Martijn Bastiaan <martijn@hmbastiaan.nl>
2016-10-21 10:45:00 +0200

    Added run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+python3 check.py  problems/test01.SWE
+echo "------------------------------"
+python3 check.py  problems/test02.SWE
+echo "------------------------------"
+python3 check.py  problems/test03.SWE
+echo "------------------------------"
+python3 check.py  problems/test04.SWE
+echo "------------------------------"
+python3 check.py  problems/test05.SWE
+echo "------------------------------"
+python3 check.py  problems/test06.SWE
